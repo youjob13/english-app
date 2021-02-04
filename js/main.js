326f1eb028a4acef7c19b6c3955ae3fdfd66cc26
Danil Rodionow <danil.rodionow13@gmail.com>
2021-02-04 10:50:13 +0300

    refactor: rewrote create elements functions

--- js/main.js
+++ js/main.js
@@ -38,37 +38,124 @@ class TasksGenerate {
   digitalRandom(min, max) {
     return Math.floor(min + Math.random() * (max + 1 - min));
   }
+  createCloseBtn() {
+    const closeBtn = document.createElement("div");
+    closeBtn.classList.add("close-btn");
+    return closeBtn;
+  }
+  openMenu() {
+    app.innerHTML = "";
+    const menu = document.createElement("div");
+    menu.classList.add("menu");
+
+    const h1 = document.createElement("h1");
+    h1.classList.add("menu-title");
+    h1.textContent = "Welcome!";
+
+    const menuButtons = document.createElement("div");
+    menuButtons.classList.add("menu-buttons");
+
+    const startBtn = document.createElement("button");
+    startBtn.classList.add("menu-btn");
+    startBtn.id = "start";
+    startBtn.textContent = "Start learning";
+
+    const vocabularyBtn = document.createElement("button");
+    vocabularyBtn.classList.add("menu-btn");
+    vocabularyBtn.id = "vocabulary";
+    vocabularyBtn.textContent = "Vocabulary";
+
+    menuButtons.append(startBtn);
+    menuButtons.append(vocabularyBtn);
+    menu.append(h1);
+    menu.append(menuButtons);
+    menu.append(this.createCloseBtn());
+    app.append(menu);
+  }
   giveResult() {
-    const result = `
-    <div class='result-block'>
-      <h2>Decision result</h2>
-      <div class='result-score'>
-        <p>Right answers - ${this.rightAnswers.length}</p>
-        <p>Wrong asnwers - ${this.loseAnswers.length}</p>
-        <p>Skiped answers - ${this.skipAnswers.length}</p>
-      </div>
-      <div class='result-buttons'>
-        <button id='try-again'>Try again</button>
-        <button id='show-result'>Show results</button>
-        <button id='go-to-menu'>Menu</button>
-      </div>
-    </div>`;
-    app.innerHTML = result;
+    const resultBlock = document.createElement("div");
+    resultBlock.classList.add("result-block");
+
+    const h2 = document.createElement("h2");
+    h2.textContent = "Decision result";
+
+    const resultScore = document.createElement("div");
+    resultScore.classList.add("result-score");
+
+    const pRight = document.createElement("p");
+    pRight.textContent = `Right - ${this.rightAnswers.length}`;
+
+    const pWrong = document.createElement("p");
+    pWrong.textContent = `Wrong - ${this.loseAnswers.length}`;
+
+    const pPassed = document.createElement("p");
+    pPassed.textContent = `Passed - ${this.skipAnswers.length}`;
+
+    const resultButtons = document.createElement("div");
+    resultButtons.classList.add("result-buttons");
+
+    const tryAgainBtn = document.createElement("button");
+    tryAgainBtn.id = "try-again";
+    tryAgainBtn.textContent = "Try again";
+
+    const showResultBtn = document.createElement("button");
+    showResultBtn.id = "show-result";
+    showResultBtn.textContent = "Show results";
+
+    const goToMenuBtn = document.createElement("button");
+    goToMenuBtn.id = "go-to-menu";
+    goToMenuBtn.textContent = "Menu";
+
+    resultButtons.append(tryAgainBtn);
+    resultButtons.append(showResultBtn);
+    resultButtons.append(goToMenuBtn);
+
+    resultScore.append(pRight);
+    resultScore.append(pWrong);
+    resultScore.append(pPassed);
+
+    resultBlock.append(h2);
+    resultBlock.append(resultScore);
+    resultBlock.append(resultButtons);
+
+    resultBlock.append(this.createCloseBtn());
+    app.append(resultBlock);
   }
   askQuestion(question) {
     if (!this.swapsArr[0]) {
       this.giveResult();
     } else {
-      const questionBlock = `
-      <div class='question-block'>
-        <h2>${question.word}</h2>
-        <input id='field-for-answer' type='text'>
-        <div class='question-buttons'>
-          <button id='give'>Give answer</button>
-          <button id='skip'>Skip question</button>
-        </div>
-      </div>`;
-      app.innerHTML = questionBlock;
+      app.innerHTML = "";
+      const questionBlock = document.createElement("div");
+      questionBlock.classList.add("question-block");
+
+      const h2 = document.createElement("h2");
+      h2.textContent = `${question.word}`;
+
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = "field-for-answer";
+
+      const questionButtons = document.createElement("div");
+      questionButtons.classList.add("question-buttons");
+
+      const answerBtn = document.createElement("button");
+      answerBtn.id = "give";
+      answerBtn.textContent = "Give answer";
+
+      const skipBtn = document.createElement("button");
+      skipBtn.id = "skip";
+      skipBtn.textContent = "Skip question";
+
+      questionButtons.append(answerBtn);
+      questionButtons.append(skipBtn);
+
+      questionBlock.append(h2);
+      questionBlock.append(input);
+      questionBlock.append(questionButtons);
+
+      questionBlock.append(this.createCloseBtn());
+      app.append(questionBlock);
     }
   }
   startTask() {
@@ -117,48 +204,44 @@ class TasksGenerate {
     return fragment;
   }
   showResults() {
-    // Block wrapper
     const results = document.createElement("div");
     results.classList.add("extended-results");
-    // Close button
-    const closeBtn = document.createElement("div");
-    closeBtn.classList.add("close-btn");
-    // Lists wrappers
+
     const resultsTrue = document.createElement("div");
     resultsTrue.classList.add("result-list");
     const resultsLose = document.createElement("div");
     resultsLose.classList.add("result-list");
     const resultsSkip = document.createElement("div");
     resultsSkip.classList.add("result-list");
-    // Titles
+
     const h3Right = document.createElement("h3");
     const h3Lose = document.createElement("h3");
     const h3Skip = document.createElement("h3");
     h3Right.textContent = "Right";
     h3Lose.textContent = "Wrong";
-    h3Skip.textContent = "Skiped";
-    // Lists answers
+    h3Skip.textContent = "Passed";
+
     const ulRight = document.createElement("ul");
     ulRight.append(this.getListContent(this.rightAnswers));
     const ulLose = document.createElement("ul");
     ulLose.append(this.getListContent(this.loseAnswers));
     const ulSkip = document.createElement("ul");
     ulSkip.append(this.getListContent(this.skipAnswers));
-    // Insert titles
+
     resultsTrue.append(h3Right);
     resultsLose.append(h3Lose);
     resultsSkip.append(h3Skip);
-    // insert lists
+
     h3Right.after(ulRight);
     h3Lose.after(ulLose);
     h3Skip.after(ulSkip);
-    // insert lists wrappers
+
     results.append(resultsTrue);
     results.append(resultsLose);
     results.append(resultsSkip);
-    // insert close button
-    results.append(closeBtn);
-    // insert block wrapper
+
+    results.append(this.createCloseBtn());
+
     app.append(results);
   }
   eventsListener() {
@@ -174,6 +257,9 @@ class TasksGenerate {
       }
       if (e.target.closest("#show-result")) {
         this.showResults();
+      }
+      if (e.target.closest("#go-to-menu")) {
+        this.openMenu();
       }
     });
 
